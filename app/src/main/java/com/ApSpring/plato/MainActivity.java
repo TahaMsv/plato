adcfff452174d6b92a53fd1276707ac2c92d328a
Saba <saba.eb18@gmail.com>
2020-06-24 13:16:00 +0430

    two buttons added

--- app/src/main/java/com/ApSpring/plato/MainActivity.java
+++ app/src/main/java/com/ApSpring/plato/MainActivity.java
@@ -2,13 +2,28 @@ package com.ApSpring.plato;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button signUpButton;
+    Button signInButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signInButton = findViewById(R.id.firstPageSignInButton);
+        signUpButton = findViewById(R.id.firstPageSignUpButton);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
