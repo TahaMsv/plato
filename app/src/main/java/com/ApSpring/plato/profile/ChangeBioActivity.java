606a6f367daaece5fb2c2e0ebad0b02e6c7b6da7
Saba <saba.eb18@gmail.com>
2020-07-15 15:32:31 +0430

    change pass, user, bio layouts added

---
+++ app/src/main/java/com/ApSpring/plato/profile/ChangeBioActivity.java
@@ -0,0 +1,16 @@
+package com.ApSpring.plato.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ApSpring.plato.R;
+
+public class ChangeBioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_bio);
+    }
+}
