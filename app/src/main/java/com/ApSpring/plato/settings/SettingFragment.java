e17fc46610aa7726fd9c3ff4548848241f6b903c
tahamousavi <tahamousavi.sbu@gmail.com>
2020-07-19 19:13:55 +0430

    some changes in setting fragment

--- app/src/main/java/com/ApSpring/plato/settings/SettingFragment.java
+++ app/src/main/java/com/ApSpring/plato/settings/SettingFragment.java
@@ -1,8 +1,10 @@
 package com.ApSpring.plato.settings;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -23,6 +25,7 @@ public class SettingFragment extends Fragment {
     Button logOut;
     Button aboutUs;
     View view;
+
     public SettingFragment() {
         // Required empty public constructor
     }
@@ -38,16 +41,29 @@ public class SettingFragment extends Fragment {
         logOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), MainActivity.class);
-                startActivity(intent);
+                AlertDialog.Builder b = new AlertDialog.Builder(getActivity());
+                b.setMessage("Are you sure you want to log out ?")
+                        .setCancelable(true)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent intent = new Intent(getActivity(), MainActivity.class);
+                                startActivity(intent);
+                            }
+                        })
+                        .setNegativeButton("No", null);
+                b.show();
+
             }
         });
 
         aboutUs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent = new Intent(getActivity(), AboutUs.class);
                 startActivity(intent);
+
             }
         });
 
