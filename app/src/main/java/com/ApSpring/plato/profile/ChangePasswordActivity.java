dacf0a6352500f36414f48063e013ded78a28c14
Saba <saba.eb18@gmail.com>
2020-07-20 18:37:56 +0430

    change password

--- app/src/main/java/com/ApSpring/plato/profile/ChangePasswordActivity.java
+++ app/src/main/java/com/ApSpring/plato/profile/ChangePasswordActivity.java
@@ -2,15 +2,40 @@ package com.ApSpring.plato.profile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.ApSpring.plato.NetworkHandlerThread;
 import com.ApSpring.plato.R;
 
 public class ChangePasswordActivity extends AppCompatActivity {
 
+    EditText userInputEditTxt;
+    Button userSubmitPassword;
+    String userInputNewPass;
+    NetworkHandlerThread netThread;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+        userInputEditTxt = findViewById(R.id.userInputNewPassword);
+        userSubmitPassword = findViewById(R.id.changePasswordBtn);
+        netThread = new NetworkHandlerThread();
+        netThread.start();
+
+        userSubmitPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userInputNewPass = userInputEditTxt.getText().toString();
+                netThread.sendMessage("changePassword:" + userInputNewPass);
+                Intent intent = new Intent();
+                intent.putExtra("newUsername", userInputNewPass);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
     }
 }
