2e2289990ff1bf85f902b692ab29aced990f3a52
Saba <saba.eb18@gmail.com>
2020-07-20 18:27:25 +0430

    change username

--- app/src/main/java/com/ApSpring/plato/profile/ChangeUserName.java
+++ app/src/main/java/com/ApSpring/plato/profile/ChangeUserName.java
@@ -9,16 +9,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.ApSpring.plato.NetworkHandlerThread;
 import com.ApSpring.plato.R;
 
 public class ChangeUserName extends AppCompatActivity {
     EditText newUsername;
     Button submitUsername;
     String userUsername;
+    NetworkHandlerThread netThread;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_user_name);
+        netThread = new NetworkHandlerThread();
+        netThread.start();
         newUsername = findViewById(R.id.userInputNewUserName);
         submitUsername = findViewById(R.id.newUserNameSubmit);
 
@@ -26,6 +30,7 @@ public class ChangeUserName extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 userUsername = newUsername.getText().toString();
+                netThread.sendMessage("changeUsername:" + userUsername);
                 Intent intent = new Intent();
                 intent.putExtra("newUsername", userUsername);
                 setResult(RESULT_OK, intent);
