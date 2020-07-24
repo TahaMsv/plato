7e97c307825593b3bed6ca766a35f67946af23d1
tahamousavi <tahamousavi.sbu@gmail.com>
2020-07-24 06:19:42 +0430

    profile layout fixed

--- app/src/main/java/com/ApSpring/plato/profile/ProfilePage.java
+++ app/src/main/java/com/ApSpring/plato/profile/ProfilePage.java
@@ -1,11 +1,14 @@
 package com.ApSpring.plato.profile;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ApSpring.plato.MainActivity;
@@ -20,10 +23,12 @@ public class ProfilePage extends AppCompatActivity {
     Button changePassWord;
     CircleImageView profileImage;
     TextView username;
+    Uri imageUrl;
 
 
     public static final int CHANGE_USERNAME = 1;
     public static final int CHANGE_PASSWORD = 2;
+    private static final int PICK_IMAGE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,23 +47,30 @@ public class ProfilePage extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ProfilePage.this, ChangePasswordActivity.class);
-                startActivityForResult(intent,CHANGE_PASSWORD);
+                startActivityForResult(intent, CHANGE_PASSWORD);
             }
         });
 
-        if(MainActivity.profileImageUri!=null){
+        if (MainActivity.profileImageUri != null) {
             profileImage.setImageURI(MainActivity.profileImageUri);
         }
 
         changePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                openGallery();
             }
         });
 
 
     }
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+
+    }
+
 
     private void init() {
         changePassWord = findViewById(R.id.changePasswordBtn);
@@ -78,6 +90,12 @@ public class ProfilePage extends AppCompatActivity {
                 MainActivity.username = newUsername;
                 username.setText(newUsername);
             }
+        }
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            imageUrl = data.getData();
+            MainActivity.profileImageUri = imageUrl;
+            profileImage.setImageURI(imageUrl);
+
         }
     }
 }
